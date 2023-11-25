f2c73c63bb166ef5dd7d2df2f95ebec86fe78181
Atta_Pangestu <103463281+Attta-pangestu@users.noreply.github.com>
2023-11-25 20:18:01 +0700

    menambahkan jumlah halaman crawling, list seluruh hasil pencarian

--- data-crawling.py
+++ data-crawling.py
@@ -2,23 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-def google_search(query, year=None):
-    search_url = f"https://www.google.com/search?q={query}"
-    
-    if year:
-        # Menambahkan filter tahun ke dalam query
-        search_url += f"&tbs=cdr:1,cd_min:{year},cd_max:{year}"
+def google_search(query, year_range=None, num_pages=5):
+    query = query.replace(" ", "+")
+    results_array = []
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+    for page in range(num_pages):
+        start_index = page * 10
+        search_url = f"https://www.google.com/search?q={query}&start={start_index}"
 
-    response = requests.get(search_url, headers=headers)
-    if response.status_code == 200:
-        return response.text
-    else:
-        print(f"Failed to retrieve search results. Status code: {response.status_code}")
-        return None
+        if year_range:
+            search_url += f"&tbs=cdr:1,cd_min:{year_range[0]},cd_max:{year_range[1]}"
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
+
+        response = requests.get(search_url, headers=headers)
+        if response.status_code == 200:
+            search_results = extract_links_and_titles(response.text)
+            results_array.extend(search_results)
+        else:
+            print(f"Failed to retrieve search results on page {page + 1}. Status code: {response.status_code}")
+
+    return results_array
 
 def extract_links_and_titles(html):
     soup = BeautifulSoup(html, 'html.parser')
@@ -32,22 +38,57 @@ def extract_links_and_titles(html):
 
     return results_array
 
-def main():
-    search_query = input("Masukkan query pencarian: ")
-    search_year = input("Masukkan tahun filter (opsional): ")
+def analyze_news_articles(news_articles):
+    kabupaten_count = {}
+    desa_count = {}
 
-    if search_year and not re.match(r'^\d{4}$', search_year):
-        print("Format tahun tidak valid. Gunakan format YYYY (contoh: 2022)")
+    for article in news_articles:
+        title = article['title'].lower()
+
+        if 'kabupaten' in title:
+            kabupaten_name = re.search(r'kabupaten (\w+)', title)
+            if kabupaten_name:
+                kabupaten_name = kabupaten_name.group(1)
+                kabupaten_count[kabupaten_name] = kabupaten_count.get(kabupaten_name, 0) + 1
+
+        if 'desa' in title:
+            desa_name = re.search(r'desa (\w+)', title)
+            if desa_name:
+                desa_name = desa_name.group(1)
+                desa_count[desa_name] = desa_count.get(desa_name, 0) + 1
+
+    return kabupaten_count, desa_count
+
+def main():
+    search_query = input("Masukkan query pencarian (lebih dari satu kalimat): ")
+    start_year = input("Masukkan tahun awal filter: ")
+    end_year = input("Masukkan tahun akhir filter: ")
+
+    if not re.match(r'^\d{4}$', start_year) or not re.match(r'^\d{4}$', end_year):
+        print("Format tahun tidak valid. Gunakan format YYYY (contoh: 2020)")
         return
 
-    search_html = google_search(search_query, search_year)
+    year_range = (start_year, end_year)
 
-    if search_html:
-        search_results = extract_links_and_titles(search_html)
+    num_pages = int(input("Masukkan jumlah halaman hasil pencarian yang ingin diambil: "))
 
+    search_results = google_search(search_query, year_range, num_pages)
+
+    if search_results:
         print("\nHasil Pencarian:")
         for i, result in enumerate(search_results, 1):
             print(f"{i}. {result['title']} - {result['link']}")
+
+        kabupaten_count, desa_count = analyze_news_articles(search_results)
+
+        print("\nAnalisis Berita:")
+        print("\nKabupaten:")
+        for kabupaten, count in kabupaten_count.items():
+            print(f"{kabupaten}: {count} kali disebut")
+
+        print("\nDesa:")
+        for desa, count in desa_count.items():
+            print(f"{desa}: {count} kali disebut")
 
 if __name__ == "__main__":
     main()
