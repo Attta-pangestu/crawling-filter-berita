ed52ac6f7347c597d250ec53e6220ddc85023603
Atta_Pangestu <103463281+Attta-pangestu@users.noreply.github.com>
2023-12-09 20:56:02 +0700

    menambahkan aksi untuk filter namun tidak berhasil

--- search-link-acquisition.py
+++ search-link-acquisition.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -7,19 +11,67 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 dr = webdriver.Chrome(options=options)
 
-# Melakukan pencarian Google dengan kata kunci "kkeringan"
-search_keyword = "kkeringan"
-google_search_url = f"https://www.google.com/search?q={search_keyword}"
-dr.get(google_search_url)
+# Membuka halaman pencarian Google
+dr.get("https://www.google.com")
 
-# Mencatat link hasil pencarian
-print("Hasil Pencarian untuk kata kunci:", search_keyword)
-c = 1
-for i in dr.find_elements(By.CSS_SELECTOR, 'div.tF2Cxc'):
-    link_element = i.find_element(By.CSS_SELECTOR, 'a')
-    link = link_element.get_attribute('href')
-    print(str(c) + ". " + link)
-    c += 1
+# Mengisi kotak pencarian dengan kata kunci "kkeringan"
+search_box = dr.find_element(By.NAME, 'q')
+search_box.send_keys("kekeringan")
+search_box.send_keys(Keys.RETURN)
+
+# Menemukan elemen "Alat" dan mengkliknya
+tools_element = WebDriverWait(dr, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Alat')]"))
+)
+tools_element.click()
+
+# Menunggu hingga elemen "Any time" muncul dan mengkliknya
+any_time_element = WebDriverWait(dr, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//div[contains(text(), 'Any time')]"))
+)
+any_time_element.click()
+
+# Menunggu hingga elemen "Custom range..." muncul dan mengkliknya
+custom_range_element = WebDriverWait(dr, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Custom range...')]"))
+)
+custom_range_element.click()
+
+# Menunggu sejenak untuk elemen tanggal muncul
+tanggal_awal_element = WebDriverWait(dr, 10).until(
+    EC.visibility_of_element_located((By.ID, "OouJcb"))
+)
+
+# Menemukan elemen input untuk tanggal awal dan mengisinya
+tanggal_awal_element.clear()
+tanggal_awal_element.send_keys("01/01/2023")
+
+# Menunggu sejenak untuk elemen tanggal akhir muncul
+tanggal_akhir_element = WebDriverWait(dr, 10).until(
+    EC.visibility_of_element_located((By.ID, "rzG2be"))
+)
+
+# Menemukan elemen input untuk tanggal akhir dan mengisinya
+tanggal_akhir_element.clear()
+tanggal_akhir_element.send_keys("12/31/2023")
+
+# Menunggu hingga elemen tombol "Go" muncul dan mengkliknya
+go_button = WebDriverWait(dr, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//g-button[contains(text(), 'Go')]"))
+)
+go_button.click()
+
+# Menunggu sejenak untuk hasil filter waktu diaplikasikan
+dr.implicitly_wait(3)
+
+# Menampilkan hasil pencarian setelah filter waktu
+hasil_pencarian = dr.find_elements(By.XPATH, "//div[@class='tF2Cxc']")
+for i, item in enumerate(hasil_pencarian, start=1):
+    title = item.find_element(By.XPATH, ".//h3").text
+    link = item.find_element(By.XPATH, ".//a").get_attribute('href')
+    print(f"{i}. Judul: {title}")
+    print(f"   Link: {link}")
+    print()
 
 # Quitting the browser
 dr.quit()
