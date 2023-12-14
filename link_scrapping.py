7dcd4e378069c0f65346c207453de77f6c10bf9d
Atta_Pangestu <103463281+Attta-pangestu@users.noreply.github.com>
2023-12-14 23:29:19 +0700

    replit program scraping and updating through gsheets

--- link_scrapping.py
+++ link_scrapping.py
@@ -1,104 +1,119 @@
+from pygsheets.worksheet import Worksheet
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import time
+import pygsheets
+from pygsheets.exceptions import WorksheetNotFound
+import pandas as pd
 
-from bs4 import BeautifulSoup
-
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=options)
 
 links = []
 
+
 def scrape_all_links(driver):
-    # Ambil URL dari hasil pencarian
-    search_results = driver.find_elements("css selector", 'li.b_algo h2 a')
-    result_links = [result.get_attribute('href') for result in search_results]
-    
-    # Menambahkan semua tautan ke dalam array links
-    links.extend(result_links)
-    
+  # Ambil URL dari hasil pencarian
+  search_results = driver.find_elements(By.XPATH, '//li[@class="b_algo"]/h2/a')
+  result_links = [result.get_attribute('href') for result in search_results]
+
+  # Menambahkan semua tautan ke dalam array links
+  links.extend(result_links)
+
+
 def search_bing_with_pagination(search_keyword, num_pages=10):
-    # URL Bing
-    bing_url = 'https://www.bing.com/'
+  # URL Bing
+  bing_url = 'https://www.bing.com/'
 
-    # Inisialisasi driver Chrome
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+  # Inisialisasi driver Chrome
+  try:
+    # Buka Bing
+    driver.get(bing_url)
+    time.sleep(4)
+    # Temukan elemen input pencarian
+    search_box = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "q")))
 
-    try:
-        # Buka Bing
-        driver.get(bing_url)
-        time.sleep(4)
-        # Temukan elemen input pencarian
-        search_box = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.NAME, "q"))
-        )
+    # Masukkan kata kunci
+    search_box.send_keys(search_keyword)
+    time.sleep(2)
 
-        # Masukkan kata kunci
-        search_box.send_keys(search_keyword)
+    # Tekan tombol Enter
+    search_box.send_keys(Keys.ENTER)
 
-        # Tekan tombol Enter
-        search_box.send_keys(Keys.ENTER)
+    for page in range(num_pages):
+      # Tunggu hingga hasil pencarian dimuat
+      WebDriverWait(driver, 20).until(
+          EC.presence_of_element_located((By.CSS_SELECTOR, 'li.b_algo h2 a')))
 
-        time.sleep(3)
+      # Panggil fungsi untuk scrape semua link
+      scrape_all_links(driver)
 
+      time.sleep(1)
+      print(f'Didapatkan sebanyak {len(links)} tautan')
+      # # Cetak link untuk setiap halaman
+      # print(f"Hasil Pencarian untuk halaman {page + 1}:")
+      # for i, link in enumerate(links, start=1):
+      #   print(f"{i}. {link}")
+      # Coba temukan elemen untuk halaman berikutnya dan klik
+      try:
+        time.sleep(2)
+        next_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'a.sb_pagN')))
+        driver.execute_script("arguments[0].click();", next_button)
+      except Exception as e:
+        print(f"Tidak dapat menemukan elemen halaman berikutnya: {e}")
+        break  # Keluar dari loop jika tidak dapat menemukan elemen halaman berikutnya
 
-        # Ubah Filter Date Range
-        # Cari dan klik elemen "Tools"
-        tools_element = WebDriverWait(driver,10).until(
-            EC.presence_of_element_located((By.ID, 'scope_tools_wrapper'))) 
-        tools_element.click()
-
-        # Cari dan klik elemen "Date"
-        date_element = WebDriverWait(driver,10).until(
-            EC.presence_of_element_located((By.ID, 'b_tween_searchTools'))) 
-        date_element.click()
-
-        # Masukkan tanggal mulai
-        start_date_input = WebDriverWait(driver,10).until(
-            EC.presence_of_element_located((By.ID, 'date_range_start')))
-        start_date_input.clear()
-        start_date_input.send_keys('01/01/2018')
-
-        # Masukkan tanggal akhir
-        end_date_input = driver.find_element(By.ID, 'date_range_end')
-        end_date_input.clear()
-        end_date_input.send_keys('01/01/2020')
-
-        # Klik tombol "Apply"
-        apply_button = driver.find_element(By.ID, 'time_filter_done_link')
-        apply_button.click()
-
-        for page in range(num_pages):
-            # Tunggu hingga hasil pencarian dimuat
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, 'li.b_algo h2 a'))
-            )
-
-            # Panggil fungsi untuk scrape semua link
-            scrape_all_links(driver)
-
-            # Cetak link untuk setiap halaman
-            print(f"Hasil Pencarian untuk halaman {page + 1}:")
-            for i, link in enumerate(links, start=1):
-                print(f"{i}. {link}")
-
-            # Coba temukan elemen untuk halaman berikutnya dan klik
-            try:
-                next_button = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.sb_pagN'))
-                )
-                next_button.click()
-            except Exception as e:
-                print(f"Tidak dapat menemukan elemen halaman berikutnya: {e}")
-                break  # Keluar dari loop jika tidak dapat menemukan elemen halaman berikutnya
+  finally:
+    # Tutup browser setelah selesai
+    driver.quit()
 
 
-    finally:
-        # Tutup browser setelah selesai
-        driver.quit()
+def gsheet_upload(array_link):
+  try:
+    # Create the Client
+    client = pygsheets.authorize(
+        service_account_file="kajian-ufuk-indonesia-b1e63d302e2e.json")
 
-# Panggil fungsi pencarian dengan paginasi
-search_bing_with_pagination('pemilu', num_pages=30)
+    # opens a spreadsheet by its name/title
+    spreadsht = client.open("KUI_analysis_Scraping")
+
+    # opens a worksheet by its name/title
+    worksht = spreadsht.worksheet("title", "Sheet1")
+
+    # Now, let's add data to our worksheet
+
+    # Creating the first column
+    worksht.cell("A1").set_text_format("bold", True).value = "Item"
+
+    # if updating multiple values, the data
+    # should be in a matrix format
+    link_matrix = [[link] for link in array_link]
+    worksht.update_values("A2", link_matrix)  # Adding row values
+
+    print("Links uploaded to Google Sheets successfully!")
+
+  except Exception as e:
+    print(f"Error uploading to Google Sheets: {e}")
+
+
+try:
+  # Panggil fungsi pencarian dengan paginasi
+  search_bing_with_pagination('kekeringan berita tahun 2018 sampai 2023',
+                              num_pages=400)
+  # Update to Google Sheets
+  gsheet_upload(links)
+
+except Exception as main_error:
+  gsheet_upload(links)
+  print(f"Main program encountered an error: {main_error}")
+finally:
+  # Tutup browser setelah selesai
+  driver.quit()
